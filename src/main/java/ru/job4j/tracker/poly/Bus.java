83c380dcccd14287a34d360c0b0a7c1800c489c3
Andrew <andreymustafin.it@gmail.com>
2021-10-31 22:05:45 +0300

    3. Интерфейс Input [#181092 #222635] fix1

--- src/main/java/ru/job4j/tracker/poly/Bus.java
+++ src/main/java/ru/job4j/tracker/poly/Bus.java
@@ -1,17 +1,21 @@
 package ru.job4j.tracker.poly;
 
 public class Bus implements Transport {
+    private int passengerCount;
+
     @Override
     public void drive() {
+        System.out.println("The " + getClass().getSimpleName()
+                + " is on the line");
     }
 
     @Override
     public void passengers(int passengersCount) {
+        System.out.println("On board " + passengerCount + " people");
     }
 
     @Override
     public int tankUp(int volume) {
-        int price = 56;
-        return price;
+        return 56 * volume;
     }
 }
