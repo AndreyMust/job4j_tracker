2a42534fc26efa33b8354e9afcaa9cf15306accc
Andrew <andreymustafin.it@gmail.com>
2021-11-10 22:50:38 +0300

    0. Сортировка [#10096 #225507] fix1

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,9 +2,11 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Item implements Comparable<Item> {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter
+                                    .ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
@@ -52,5 +54,22 @@ public class Item implements Comparable<Item> {
     @Override
     public int compareTo(Item another) {
         return Integer.compare(id, another.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return false;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Item item = (Item) obj;
+        return id == item.id && Objects.equals(name, item.name);
     }
 }
--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -18,4 +18,58 @@ public class ItemTest {
         Collections.sort(items);
         assertThat(items.get(0).getName(), is("CCC"));
     }
+
+    @Test
+    public void compareToUp() {
+        List<Item> items = Arrays.asList(
+                new Item(3, "Gamma"),
+                new Item(4, "Foxy"),
+                new Item(1, "Alpha"),
+                new Item(2, "Beta")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item(1, "Alpha"),
+                new Item(2, "Beta"),
+                new Item(3, "Gamma"),
+                new Item(4, "Foxy")
+        );
+        Collections.sort(items);
+        assertThat(items, is(expected));
+    }
+
+    @Test
+    public void compareUpByName() {
+        List<Item> items = Arrays.asList(
+                new Item(3, "Cetta"),
+                new Item(4, "Delta"),
+                new Item(1, "Alpha"),
+                new Item(2, "Bravo")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item(1, "Alpha"),
+                new Item(2, "Bravo"),
+                new Item(3, "Cetta"),
+                new Item(4, "Delta")
+        );
+        Collections.sort(items, new ItemSorterByName());
+        assertThat(items, is(expected));
+    }
+
+    @Test
+    public void compareUpByNameDesc() {
+        List<Item> items = Arrays.asList(
+                new Item(3, "Cetta"),
+                new Item(4, "Delta"),
+                new Item(1, "Alpha"),
+                new Item(2, "Bravo")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item(4, "Delta"),
+                new Item(3, "Cetta"),
+                new Item(2, "Bravo"),
+                new Item(1, "Alpha")
+        );
+        Collections.sort(items, new ItemSorterByNameDesc());
+        assertThat(items, is(expected));
+    }
 }
